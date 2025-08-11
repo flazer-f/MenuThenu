b88604788ecabd254c077142c19fc4078e8ed7a7
flazer <flazerdourado@gmail.com>
2025-08-11 23:38:34 +0530

    working changes react issue resolved

--- MenuThenu/src/main.jsx
+++ MenuThenu/src/main.jsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react'
+import React,{ StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import CreateMenu from "./components/createmenu/CreateMenu";
+import "./index.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
