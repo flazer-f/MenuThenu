816bf6af288719e32099a560ed4b1f405179db1d
JorgeDev-stack <jesus.jorge9810@gmail.com>
2025-08-20 01:17:33 +0530

    Add Carousel component with CSS styling and functionality
    - Implemented Carousel component in Carousel.jsx
    - Added auto-slide feature with interval of 4 seconds
    - Included navigation buttons for manual slide control
    - Added dot indicators for slide navigation
    - Created Carousel.css for styling the carousel layout and elements

--- MenuThenu/src/AppRoutes.jsx
+++ MenuThenu/src/AppRoutes.jsx
@@ -4,11 +4,13 @@ import MenuCreator from './components/createmenu/MenuCreator';
 import Dashboard from './components/dashboard/AdminDashboard';
 import MenuList from './components/dashboard/MenuList'
 import Layout from './components/dashboard/Layout'
+import HomePage from './components/homepage/HomePage';
 
 
 const appRoutes = [
+  { path: "/", element: <HomePage /> },     // Homepage route
   { path: '/', element: <MenuCreator /> },
-   {
+  {
     path: "/admin",       // base route for layout
     element: <Layout />,
     children: [
