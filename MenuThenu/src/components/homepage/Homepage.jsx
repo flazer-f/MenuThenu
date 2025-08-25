394069ddc42b8782527d1b3c99c845ca03aa65b6
LachlanEthanvas <166016001+LachlanEthanvas@users.noreply.github.com>
2025-08-25 21:18:21 +0530

    refactor: Simplify HomePage component to serve as a container for routed content

--- MenuThenu/src/components/homepage/Homepage.jsx
+++ MenuThenu/src/components/homepage/Homepage.jsx
@@ -1,15 +1,6 @@
 import React from "react";
-import Header from "./Header";
-import Hero from "./Hero";
-import Footer from "./Footer";
-
+import { Outlet } from "react-router-dom";
 
 export default function HomePage() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <Footer />
-    </>
-  );
+  return <Outlet />;
 }
--- MenuThenu/src/components/homepage/homepage.jsx
+++ MenuThenu/src/components/homepage/homepage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-// HomePage now just acts as a container for the content being rendered by the router
 export default function HomePage() {
   return <Outlet />;
 }
